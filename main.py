354c61087f78aab6576509ada69c6512fd11e618
HarcicYang <84220553+HarcicYang@users.noreply.github.com>
2023-01-29 21:09:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import os
-import sys, zipfile, shutil, win32con, win32api, atexit
+import sys, zipfile, shutil, win32con, win32api, atexit, tzlocal, os
 from PIL import Image
 from tkinter import filedialog
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 atexit.register(lambda: shutil.rmtree("./temp"))
+localTimeZone = tzlocal.get_localzone()
 
 
 class Ui_MainWindow(object):
@@ -156,19 +156,19 @@ class Ui_MainWindow(object):
                                        "}")
         self.progressBar.setProperty("value", 0)
         self.progressBar.setObjectName("progressBar")
-        self.ProcessBar = QtWidgets.QLabel(self.centralwidget)
-        self.ProcessBar.setGeometry(QtCore.QRect(20, 200, 540, 61))
+        self.ProgressBar = QtWidgets.QLabel(self.centralwidget)
+        self.ProgressBar.setGeometry(QtCore.QRect(20, 200, 540, 61))
         font = QtGui.QFont()
         font.setFamily("Microsoft YaHei UI")
-        self.ProcessBar.setFont(font)
-        self.ProcessBar.setStyleSheet("QLabel {\n"
-                                      "    color: white;\n"
-                                      "    background-color: rgb(39, 39, 39);\n"
-                                      "    border: 0px;\n"
-                                      "    border-radius: 7px;\n"
-                                      "}")
-        self.ProcessBar.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
-        self.ProcessBar.setObjectName("ProcessBar")
+        self.ProgressBar.setFont(font)
+        self.ProgressBar.setStyleSheet("QLabel {\n"
+                                       "    color: white;\n"
+                                       "    background-color: rgb(39, 39, 39);\n"
+                                       "    border: 0px;\n"
+                                       "    border-radius: 7px;\n"
+                                       "}")
+        self.ProgressBar.setAlignment(QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
+        self.ProgressBar.setObjectName("ProgressBar")
         self.ChooseButton = QtWidgets.QPushButton(self.centralwidget)
         self.ChooseButton.setGeometry(QtCore.QRect(110, 170, 41, 16))
         font = QtGui.QFont()
@@ -210,7 +210,7 @@ class Ui_MainWindow(object):
         self.FileNameInput.raise_()
         self.PackName.raise_()
         self.comboBox.raise_()
-        self.ProcessBar.raise_()
+        self.ProgressBar.raise_()
         self.progressBar.raise_()
         self.ChooseButton.raise_()
         self.pushButton.raise_()
@@ -220,33 +220,64 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        self.translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(self.translate("MainWindow", "Pak converter"))
-        self.Title.setText(self.translate("MainWindow", "Resourcepacks resolution converter"))
-        self.Title_2.setText(self.translate("MainWindow", "资源包分辨率修改 By HarcicYang"))
-        self.label.setText(self.translate("MainWindow", "已选择的资源包"))
-        self.label_2.setText(self.translate("MainWindow", "输出选项"))
-        self.ResolutionText.setText(self.translate("MainWindow", "分辨率"))
-        self.FileText.setText(self.translate("MainWindow", "文件名"))
-        self.FileNameInput.setText(self.translate("MainWindow", "new.zip"))
-        self.comboBox.setCurrentText(self.translate("MainWindow", "128x"))
-        self.comboBox.setItemText(0, self.translate("MainWindow", "16x"))
-        self.comboBox.setItemText(1, self.translate("MainWindow", "32x"))
-        self.comboBox.setItemText(2, self.translate("MainWindow", "64x"))
-        self.comboBox.setItemText(3, self.translate("MainWindow", "128x"))
-        self.comboBox.setItemText(4, self.translate("MainWindow", "256x"))
-        self.comboBox.setItemText(5, self.translate("MainWindow", "512x"))
-        self.comboBox.setItemText(6, self.translate("MainWindow", "1024x"))
-        self.comboBox.setItemText(7, self.translate("MainWindow", "2048x"))
-        self.comboBox.setItemText(8, self.translate("MainWindow", "4096x"))
-        self.ProcessBar.setText(self.translate("MainWindow", "    输出进度"))
-        self.ChooseButton.setText(self.translate("MainWindow", "选择"))
-        self.ChooseButton.clicked.connect(self.Open)
-        self.pushButton.setText(self.translate("MainWindow", "开始"))
-        self.pushButton.clicked.connect(self.Run)
+        # 根据时区设置语言
+        if localTimeZone == "Asia/Shanghai" or "Asia/Harbin" or "Asia/Chongqing" or "Asia/Urumqi" or "Asia/Kashgar":
+            self.translate = QtCore.QCoreApplication.translate
+            MainWindow.setWindowTitle(self.translate("MainWindow", "Pak converter"))
+            self.Title.setText(self.translate("MainWindow", "Resourcepacks resolution converter"))
+            self.Title_2.setText(self.translate("MainWindow", "资源包分辨率修改 By HarcicYang"))
+            self.label.setText(self.translate("MainWindow", "已选择的资源包"))
+            self.label_2.setText(self.translate("MainWindow", "输出选项"))
+            self.ResolutionText.setText(self.translate("MainWindow", "分辨率"))
+            self.FileText.setText(self.translate("MainWindow", "文件名"))
+            self.FileNameInput.setText(self.translate("MainWindow", "new.zip"))
+            self.comboBox.setCurrentText(self.translate("MainWindow", "128x"))
+            self.comboBox.setItemText(0, self.translate("MainWindow", "16x"))
+            self.comboBox.setItemText(1, self.translate("MainWindow", "32x"))
+            self.comboBox.setItemText(2, self.translate("MainWindow", "64x"))
+            self.comboBox.setItemText(3, self.translate("MainWindow", "128x"))
+            self.comboBox.setItemText(4, self.translate("MainWindow", "256x"))
+            self.comboBox.setItemText(5, self.translate("MainWindow", "512x"))
+            self.comboBox.setItemText(6, self.translate("MainWindow", "1024x"))
+            self.comboBox.setItemText(7, self.translate("MainWindow", "2048x"))
+            self.comboBox.setItemText(8, self.translate("MainWindow", "4096x"))
+            self.ProgressBar.setText(self.translate("MainWindow", "    输出进度"))
+            self.ChooseButton.setText(self.translate("MainWindow", "选择"))
+            self.ChooseButton.clicked.connect(self.Open)
+            self.pushButton.setText(self.translate("MainWindow", "开始"))
+            self.pushButton.clicked.connect(self.Run)
+        else:
+            self.translate = QtCore.QCoreApplication.translate
+            MainWindow.setWindowTitle(self.translate("MainWindow", "Pak converter"))
+            self.Title.setText(self.translate("MainWindow", "Resourcepacks resolution converter"))
+            self.Title_2.setText(self.translate("MainWindow", "By HarcicYang"))
+            self.label.setText(self.translate("MainWindow", "Chosen resource pack"))
+            self.label_2.setText(self.translate("MainWindow", "Options"))
+            self.ResolutionText.setText(self.translate("MainWindow", "Resolution"))
+            self.FileText.setText(self.translate("MainWindow", "Output file name"))
+            self.FileNameInput.setText(self.translate("MainWindow", "new.zip"))
+            self.comboBox.setCurrentText(self.translate("MainWindow", "128x"))
+            self.comboBox.setItemText(0, self.translate("MainWindow", "16x"))
+            self.comboBox.setItemText(1, self.translate("MainWindow", "32x"))
+            self.comboBox.setItemText(2, self.translate("MainWindow", "64x"))
+            self.comboBox.setItemText(3, self.translate("MainWindow", "128x"))
+            self.comboBox.setItemText(4, self.translate("MainWindow", "256x"))
+            self.comboBox.setItemText(5, self.translate("MainWindow", "512x"))
+            self.comboBox.setItemText(6, self.translate("MainWindow", "1024x"))
+            self.comboBox.setItemText(7, self.translate("MainWindow", "2048x"))
+            self.comboBox.setItemText(8, self.translate("MainWindow", "4096x"))
+            self.ProgressBar.setText(self.translate("MainWindow", "    Progress"))
+            self.ChooseButton.setText(self.translate("MainWindow", "Choose one"))
+            self.ChooseButton.clicked.connect(self.Open)
+            self.pushButton.setText(self.translate("MainWindow", "Start"))
+            self.pushButton.clicked.connect(self.Run)
 
     def Open(self):  # 文件打开方法
-        filePath = filedialog.askopenfilename(title="选择一个资源包", filetypes=(("资源包", "*.zip"),))
+        if localTimeZone == "Asia/Shanghai" or "Asia/Harbin" or "Asia/Chongqing" or "Asia/Urumqi" or "Asia/Kashgar":
+            filePath = filedialog.askopenfilename(title="选择一个资源包", filetypes=(("资源包", "*.zip"),))
+        else:
+            filePath = filedialog.askopenfilename(title="Choose a resource pack",
+                                                  filetypes=(("Resource pack", "*.zip"),))
         try:
             zip = zipfile.ZipFile(filePath)
             zipfile.ZipFile.extractall(zip, './temp/files/')
@@ -265,9 +296,17 @@ class Ui_MainWindow(object):
                 name = i
             self.PackName.setText(self.translate("MainWindow", name))
         except:
-            win32api.MessageBox(0,
-                                "您可能选择了一个不正确的资源包\n若您坚信您选择的资源包没有问题且此问题频繁出现，请前往GitHub反馈",
-                                "提醒", win32con.MB_ICONWARNING)
+            if localTimeZone == "Asia/Shanghai" or "Asia/Harbin" or "Asia/Chongqing" or "Asia/Urumqi" or "Asia/Kashgar":
+                win32api.MessageBox(0,
+                                    "您可能选择了一个不正确的资源包\n若您坚信您选择的资源包没有问题且此问题频繁出现，请前往GitHub仓库反馈",
+                                    "提醒", win32con.MB_ICONERROR)
+            else:
+                win32api.MessageBox(0,
+                                    "You might chose a bad resource pack.\n"
+                                    "If you believe that you have chosen a normal resource pack "
+                                    "and this problem appear again and again, "
+                                    "please go to the GitHub repository to provide feedback.",
+                                    "Notification", win32con.MB_ICONERROR)
 
     def Run(self):  # 运行
         self.OutPutFile = self.FileNameInput.text()
@@ -315,7 +354,7 @@ class Ui_MainWindow(object):
                 self.progressBar.setRange(0, len(ProcessList) + 20)
                 ValueNow = 10
                 self.progressBar.setValue(ValueNow)
-                print("processBar Update")
+                print("ProgressBar Update")
                 for j in ProcessList:
                     print("processing: " + j)
                     img = Image.open(j)
@@ -325,13 +364,13 @@ class Ui_MainWindow(object):
                         print("saved: " + j + "#" + str(width) + "x" + str(int(height)))
                         ValueNow += 1
                         self.progressBar.setValue(ValueNow)
-                        print("processBar Update")
+                        print("ProgressBar Update")
                     elif img.width == img.height:
                         img.resize((width, width)).save(j)
                         print("saved: " + j + "#" + str(width) + "x" + str(width))
                         ValueNow += 1
                         self.progressBar.setValue(ValueNow)
-                        print("processBar Update")
+                        print("ProgressBar Update")
 
         self.progressBar.setValue(ValueNow + 10)
         zip = zipfile.ZipFile(self.OutPutFile, "w", zipfile.ZIP_DEFLATED)  # 压缩输出文件
@@ -341,7 +380,10 @@ class Ui_MainWindow(object):
             for filename in filenames:
                 zip.write(os.path.join(path, filename), os.path.join(fpath, filename))
         zip.close()
-        win32api.MessageBox(0, "完成，文件保存为" + os.getcwd() + self.OutPutFile, "提醒", win32con.MB_ICONASTERISK)
+        if localTimeZone == "Asia/Shanghai" or "Asia/Harbin" or "Asia/Chongqing" or "Asia/Urumqi" or "Asia/Kashgar":
+            win32api.MessageBox(0, "完成，文件保存为" + os.getcwd() + self.OutPutFile, "提醒", win32con.MB_ICONASTERISK)
+        else:
+            win32api.MessageBox(0, "Finish! The file already saved to " + os.getcwd() + self.OutPutFile, "Notification", win32con.MB_ICONASTERISK)
 
 
 from PyQt5.QtWidgets import QApplication, QMainWindow  # 加载Qt窗口
